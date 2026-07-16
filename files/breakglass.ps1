2fa537a92c6c90949a4a2d02689746100e2a3deb
rmassey411 <70042531+rmassey411@users.noreply.github.com>
2026-07-15 22:50:15 -0400

    update powershell script and capture output from script

--- files/breakglass.ps1
+++ files/breakglass.ps1
@@ -1,1 +1,9 @@
+param(
+    [string]$targetNode,
+    [string]$segmentServer,
+    [string]$deploymentCluster,
+    [string]$mode
+)
+
 Write-Output "Hello World!"
+Write-Output "targetNode: $targetNode, segmentServer: $segmentServer, deploymentCluster: $deploymentCluster, mode: $mode"
